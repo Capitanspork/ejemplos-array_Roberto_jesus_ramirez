f23fb5584328e0a6135ccd036f5cc8698cc22cba
Roberto Jesus Ramirez Guerrero <robertormzgro@gmail.com>
2021-06-01 12:47:57 -0500

    creacion de scripts y sprays

--- ejemplos array/Assets/scripts/balls.cs
+++ ejemplos array/Assets/scripts/balls.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EjemplosArray : MonoBehaviour
+public class balls : MonoBehaviour
 {
-    public GameObject[] Ballsarrray;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject)
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (input.GetKeyDown("space"))
-        {
-            Instantiate(Ballsarrray(Random.Range(0, Ballsarrray.Length));
-        }
         
     }
 }
